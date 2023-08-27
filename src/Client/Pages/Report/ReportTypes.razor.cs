9705909d8f8caf4f64c07f344cc7ff85ae97e7ca
ZafrullahId <zafrullahdaprince@gmail.com>
2023-08-27 12:25:14 +0100

    commit

--- src/Client/Pages/Report/ReportTypes.razor.cs
+++ src/Client/Pages/Report/ReportTypes.razor.cs
@@ -1,4 +1,4 @@
-﻿/*using FSH.WebApi.Shared.Authorization;
+﻿using FSH.WebApi.Shared.Authorization;
 using Mapster;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
@@ -11,73 +11,50 @@ namespace rmsweb.Client.Pages.Report;
 public partial class ReportTypes
 {
     [Inject]
-    protected IProductsClient ProductsClient { get; set; } = default!;
-    [Inject]
-    protected IBrandsClient BrandsClient { get; set; } = default!;
+    protected IReportTypesClient ReportTypesClient { get; set; } = default!;
 
-    protected EntityServerTableContext<ProductDto, Guid, ProductViewModel> Context { get; set; } = default!;
+    protected EntityServerTableContext<ReportTypeDto, Guid, ReportTypeViewModel> Context { get; set; } = default!;
     private bool _canViewRoles;
 
-    private EntityTable<ProductDto, Guid, ProductViewModel> _table = default!;
+    private EntityTable<ReportTypeDto, Guid, ReportTypeViewModel> _table = default!;
 
     protected override void OnInitialized() =>
         Context = new(
-            entityName: L["Product"],
-            entityNamePlural: L["Products"],
-            entityResource: FSHResource.Products,
+            entityName: L["ReportType"],
+            entityNamePlural: L["ReportTypes"],
+            // entityResource: FSHResource.Products,
             fields: new()
             {
                 new(prod => prod.Id, L["Id"], "Id"),
                 new(prod => prod.Name, L["Name"], "Name"),
-                new(prod => prod.BrandName, L["Category"], "Brand.Name"),
                 new(prod => prod.Description, L["Description"], "Description"),
-                new(prod => prod.Rate, L["Number of Sections"], "Rate")
             },
             enableAdvancedSearch: true,
-            idFunc: prod => prod.Id,
+            idFunc: reportType => reportType.Id,
             searchFunc: async filter =>
             {
-                var productFilter = filter.Adapt<SearchProductsRequest>();
-
-                productFilter.BrandId = SearchBrandId == default ? null : SearchBrandId;
-                productFilter.MinimumRate = SearchMinimumRate;
-                productFilter.MaximumRate = SearchMaximumRate;
-
-                var result = await ProductsClient.SearchAsync(productFilter);
-                return result.Adapt<PaginationResponse<ProductDto>>();
+                var result = await ReportTypesClient.GetReportTypesAsync();
+                return result.adapt<paginationresponse<productdto>>();
             },
             createFunc: async prod =>
             {
-                if (!string.IsNullOrEmpty(prod.ImageInBytes))
-                {
-                    prod.Image = new FileUploadRequest() { Data = prod.ImageInBytes, Extension = prod.ImageExtension ?? string.Empty, Name = $"{prod.Name}_{Guid.NewGuid():N}" };
-                }
-
-                await ProductsClient.CreateAsync(prod.Adapt<CreateProductRequest>());
-                prod.ImageInBytes = string.Empty;
+                await ReportTypesClient.CreateReportTypeAsync(prod.Adapt<CreateReportTypeRequest>());
             },
             updateFunc: async (id, prod) =>
             {
-                if (!string.IsNullOrEmpty(prod.ImageInBytes))
-                {
-                    prod.DeleteCurrentImage = true;
-                    prod.Image = new FileUploadRequest() { Data = prod.ImageInBytes, Extension = prod.ImageExtension ?? string.Empty, Name = $"{prod.Name}_{Guid.NewGuid():N}" };
-                }
+                await ReportTypesClient.UpdateReportTypeAsync(id, prod.Adapt<UpdateReportTypeRequest>());
+            });
+            //exportFunc: async filter =>
+            //{
+            //    var exportFilter = filter.Adapt<ExportProductsRequest>();
 
-                await ProductsClient.UpdateAsync(id, prod.Adapt<UpdateProductRequest>());
-                prod.ImageInBytes = string.Empty;
-            },
-            exportFunc: async filter =>
-            {
-                var exportFilter = filter.Adapt<ExportProductsRequest>();
+            //    exportFilter.BrandId = SearchBrandId == default ? null : SearchBrandId;
+            //    exportFilter.MinimumRate = SearchMinimumRate;
+            //    exportFilter.MaximumRate = SearchMaximumRate;
 
-                exportFilter.BrandId = SearchBrandId == default ? null : SearchBrandId;
-                exportFilter.MinimumRate = SearchMinimumRate;
-                exportFilter.MaximumRate = SearchMaximumRate;
-
-                return await ProductsClient.ExportAsync(exportFilter);
-            },
-            deleteFunc: async id => await ProductsClient.DeleteAsync(id));
+            //    return await ProductsClient.ExportAsync(exportFilter);
+            //},
+            //deleteFunc: async id => await ProductsClient.DeleteAsync(id));
 
     // Advanced Search
 
@@ -114,42 +91,6 @@ public partial class ReportTypes
         }
     }
 
-    // TODO : Make this as a shared service or something? Since it's used by Profile Component also for now, and literally any other component that will have image upload.
-    // The new service should ideally return $"data:{ApplicationConstants.StandardImageFormat};base64,{Convert.ToBase64String(buffer)}"
-    private async Task UploadFiles(InputFileChangeEventArgs e)
-    {
-        if (e.File != null)
-        {
-            string? extension = Path.GetExtension(e.File.Name);
-            if (!ApplicationConstants.SupportedImageFormats.Contains(extension.ToLower()))
-            {
-                Snackbar.Add("Image Format Not Supported.", Severity.Error);
-                return;
-            }
-
-            Context.AddEditModal.RequestModel.ImageExtension = extension;
-            var imageFile = await e.File.RequestImageFileAsync(ApplicationConstants.StandardImageFormat, ApplicationConstants.MaxImageWidth, ApplicationConstants.MaxImageHeight);
-            byte[]? buffer = new byte[imageFile.Size];
-            await imageFile.OpenReadStream(ApplicationConstants.MaxAllowedSize).ReadAsync(buffer);
-            Context.AddEditModal.RequestModel.ImageInBytes = $"data:{ApplicationConstants.StandardImageFormat};base64,{Convert.ToBase64String(buffer)}";
-            Context.AddEditModal.ForceRender();
-        }
-    }
-
-    public void ClearImageInBytes()
-    {
-        Context.AddEditModal.RequestModel.ImageInBytes = string.Empty;
-        Context.AddEditModal.ForceRender();
-    }
-
-    public void SetDeleteCurrentImageFlag()
-    {
-        Context.AddEditModal.RequestModel.ImageInBytes = string.Empty;
-        Context.AddEditModal.RequestModel.ImagePath = string.Empty;
-        Context.AddEditModal.RequestModel.DeleteCurrentImage = true;
-        Context.AddEditModal.ForceRender();
-    }
-
     private void ViewReport(in Guid reportTypeId) =>
        Navigation.NavigateTo($"/reportTypes/{reportTypeId}");
 
@@ -157,9 +98,16 @@ public partial class ReportTypes
         Navigation.NavigateTo($"/reportTypes/{reportTypeId}/sections");
 }
 
-public class ProductViewModel : UpdateProductRequest
+public class ReportTypeViewModel
 {
-    public string? ImagePath { get; set; }
-    public string? ImageInBytes { get; set; }
-    public string? ImageExtension { get; set; }
-}*/
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+}
+
+public class ReportTypeDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+}
