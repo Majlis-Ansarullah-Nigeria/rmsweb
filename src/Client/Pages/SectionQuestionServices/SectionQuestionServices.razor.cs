349149eb00bca9306cc6d19fb66069c57d8dc74c
Timehin <timehinfarhaat@gmail.com>
2023-08-27 16:04:10 +0100

    in progress

---
+++ src/Client/Pages/SectionQuestionServices/SectionQuestionServices.razor.cs
@@ -0,0 +1,87 @@
+﻿using FSH.WebApi.Shared.Authorization;
+using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor;
+using rmsweb.Client.Components.EntityTable;
+using rmsweb.Client.Infrastructure.ApiClient;
+using rmsweb.Client.Infrastructure.Auth;
+using rmsweb.Client.Shared;
+using System.Runtime.CompilerServices;
+
+namespace rmsweb.Client.Pages.SectionQuestionServices;
+
+public partial class SectionQuestionServices
+{
+
+    [CascadingParameter]
+    protected Task<AuthenticationState> AuthState { get; set; } = default!;
+    [Inject]
+    protected IAuthorizationService AuthService { get; set; } = default!;
+    [Parameter]
+    public Guid ReportTypeId { get; set; }
+
+    [Inject]
+    protected ISectionQuestionServicesClient SectionQuestionServicesClient { get; set; } = default!;
+
+    protected EntityServerTableContext<ReportQuestionsModel, Guid, ReportQuestionRequest> Context { get; set; } = default!;
+
+    private string _title = string.Empty;
+    private string _description = string.Empty;
+
+    private string _searchString = string.Empty;
+
+    private bool _canEditUsers;
+    private bool _canSearchRoles;
+    private bool _loaded;
+    private bool _canExportUsers;
+    private bool _canViewRoles;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var user = (await AuthState).User;
+        _canExportUsers = await AuthService.HasPermissionAsync(user, FSHAction.Export, FSHResource.Users);
+        _canViewRoles = await AuthService.HasPermissionAsync(user, FSHAction.View, FSHResource.UserRoles);
+
+        Context = new(
+         entityName: L["Question"],
+            entityNamePlural: L["Questions"],
+            entityResource: FSHResource.Questions,
+            fields: new()
+            {
+                new(question => question.Text, L["Detail"]),
+                new(question => question.Point, L["Point"])
+
+            },
+            idFunc: question => question.ReportTypeId,
+            hasExtraActionsFunc: () => true,
+            canUpdateEntityFunc: e => false,
+            createAction: string.Empty,
+            canDeleteEntityFunc: e => false,
+            searchFunc: async filter =>
+            {
+                var returnObject = new PaginationResponse<ReportQuestionsModel>();
+                var result = await SectionQuestionServicesClient.GetReportTypeQuestionsAsync(ReportTypeId, "1");
+                return result.Data.Adapt<PaginationResponse<ReportQuestionsModel>>();
+            },
+            exportAction: string.Empty);
+
+    }
+
+    private void ViewProfile(in Guid questionId) =>
+       Navigation.NavigateTo($"/sectionquestionservices/{questionId}/EditQuestion");
+
+    private void ManageRoles(in Guid userId) =>
+       Navigation.NavigateTo($"/users/{userId}/roles");
+
+}
+
+
+
+
+
+
+
+
+
